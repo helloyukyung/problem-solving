8fb5e3db7f0a417859ad3946d4b1418d1a4f63af
yukyung123 <rladbrud12@naver.com>
2021-10-20 21:49:42 +0900

    2021-10-20

--- Baekjoon/1546.py
+++ Baekjoon/1546.py
@@ -3,18 +3,17 @@ input = sys.stdin.readline
 
 N= int(input())
 score_list = list(map(int, input().split()))
-Max_score = score_list[0]
+max_score = score_list[0]
 total=0
 
 for score in score_list:
-    if score > Max_score :
-        Max_score = score 
+    if score > max_score :
+        max_score = score 
 
 for i in score_list:
-    sum_score =i /Max_score*100
+    sum_score = i /max_score*100
     total +=sum_score
 
         
-
 
 print(total/N)
