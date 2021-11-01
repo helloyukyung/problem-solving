5071544267cc4b514713692b563ac50ab8fdf4dc
yukyung123 <rladbrud12@naver.com>
2021-11-01 11:31:46 +0900

    2021-11-01(5622-refactoring)

--- Baekjoon/5622.py
+++ Baekjoon/5622.py
@@ -1,27 +1,43 @@
+## refactoring
 import sys 
 input = sys.stdin.readline 
 
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
-alphabet= alphabet.upper()
-number = input()
-count = 0
+word  =input().lower()
+num = ['abc','def','ghi','jkl','mno','pqrs','tuv','wxyz']
+count =0 
 
-
-for i in range(len(number)) :
-    if number[i] in alphabet[0:3]:
-        count += 2+1
-    elif number[i] in alphabet[3:6]:
-        count += 3+1
-    elif number[i] in alphabet[6:9]:
-        count += 4+1
-    elif number[i] in alphabet[9:12]:
-        count += 5+1
-    elif number[i] in alphabet[12:15]:
-        count += 6+1
-    elif number[i] in alphabet[15:19]:
-        count += 7+1
-    elif number[i] in alphabet[19:22]:
-        count += 8+1
-    elif number[i] in alphabet[22:26]:
-        count += 9+1
+for i in word:
+    for j in num: 
+        if i in j:
+            count += num.index(j)+3
 print(count)
+
+
+# #처음 짠 코드 
+# import sys 
+# input = sys.stdin.readline 
+
+# alphabet = 'abcdefghijklmnopqrstuvwxyz'
+# alphabet= alphabet.upper()
+# number = input()
+# count = 0
+
+
+# for i in range(len(number)) :
+#     if number[i] in alphabet[0:3]:
+#         count += 2+1
+#     elif number[i] in alphabet[3:6]:
+#         count += 3+1
+#     elif number[i] in alphabet[6:9]:
+#         count += 4+1
+#     elif number[i] in alphabet[9:12]:
+#         count += 5+1
+#     elif number[i] in alphabet[12:15]:
+#         count += 6+1
+#     elif number[i] in alphabet[15:19]:
+#         count += 7+1
+#     elif number[i] in alphabet[19:22]:
+#         count += 8+1
+#     elif number[i] in alphabet[22:26]:
+#         count += 9+1
+# print(count)
