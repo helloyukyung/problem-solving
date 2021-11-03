73c9dea90137e6bf7d6c33df5e799f5147d74c2a
yukyung123 <rladbrud12@naver.com>
2021-11-03 10:25:38 +0900

    2021-11-03

---
+++ Baekjoon/1316.py
@@ -0,0 +1,11 @@
+result = int(input())
+
+for i in range(0,result):
+    word = input()
+    for j in range(0,len(word)-1):
+        if word[j] == word[j+1]:
+            pass
+        elif word[j] in word[j+1:]:
+            result-=1
+            break
+print(result)
