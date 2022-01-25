975c6370c974289dafe69abc6fd76a2d45d2e748
yukyung123 <rladbrud12@naver.com>
2022-01-25 17:21:55 +0900

    1697

--- Baekjoon/2667.py
+++ Baekjoon/2667.py
@@ -13,7 +13,7 @@ dx = [0,0,1,-1]
 dy = [1,-1,0,0]
 
 def dfs(x,y):
-    if x < 0 or x >=n or y<0 or y >=n:
+    if x < 0 or x >= n or y< 0 or y >= n:
         return False
     
     if graph[x][y] == 1:
@@ -21,16 +21,14 @@ def dfs(x,y):
         count += 1
         graph[x][y] = 0 
         for i in range(4):
-            nx = x +dx[i]
-            ny = y +dy[i]
+            nx = x + dx[i]
+            ny = y + dy[i]
             dfs(nx,ny)
         return True
     return False
 
 count = 0 
 result = 0 
-
-
 
 for i in range(n):
     for j in range(n):
