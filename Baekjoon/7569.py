4100aa5ac3383a0e48c51dd57c954486b09f21b1
yukyung123 <rladbrud12@naver.com>
2022-01-27 18:31:00 +0900

    7569

---
+++ Baekjoon/7569.py
@@ -0,0 +1,41 @@
+import sys
+from collections import deque
+m,n,h = map(int,input().split()) 
+
+graph = []
+queue = deque([])
+ 
+for i in range(h):
+    tmp = []
+    for j in range(n):
+        tmp.append(list(map(int,sys.stdin.readline().split())))
+        for k in range(m):
+            if tmp[j][k]==1:
+                queue.append([i,j,k])
+    graph.append(tmp)
+
+dx = [-1,1,0,0,0,0]
+dy = [0,0,1,-1,0,0]
+dz = [0,0,0,0,1,-1]
+
+while(queue):
+    x,y,z = queue.popleft()
+    
+    for i in range(6):
+        nx = dx[i] +x
+        ny = dy[i] +y
+        nz = dz[i] +z
+        if 0<= nx <h and 0<= ny <n and 0<= nz <m and graph[nx][ny][nz]==0:
+            queue.append([nx,ny,nz])
+            graph[nx][ny][nz] = graph[x][y][z]+1
+            
+day = 0
+
+for i in graph:
+    for j in i:
+        for k in j:
+            if k==0:
+                print(-1)
+                exit(0)
+        day = max(day,max(j))
+print(day-1)
