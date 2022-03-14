04ebdb72bfa6de2a753a6924346f301d703d2776
yukyung123 <rladbrud12@naver.com>
2022-03-14 23:23:42 +0900

    1260

--- Baekjoon/1260.py
+++ Baekjoon/1260.py
@@ -1,5 +1,5 @@
-import sys
 from collections import deque
+import sys 
 input = sys.stdin.readline
 
 def dfs(num):
@@ -7,7 +7,6 @@ def dfs(num):
     print(num, end=" ")
     for i in sorted_graph[num]:
         if dfs_visited[i] == False:
-            dfs_visited[i] = True
             dfs(i)
 
 
@@ -22,22 +21,25 @@ def bfs(num):
                 queue.append(i)
                 bfs_visited[i] = True
 
-N, M, v = map(int,input().split())
-bfs_visited = [False]*(N+1)
-dfs_visited = [False]*(N+1)
 
-graph =[[] for _ in range(N +1)]
-for i in range(1,M+1):
-    node1, node2 = map(int, input().split())
-    graph[node1].append(node2)
-    graph[node2].append(node1)
+N, M, V = map(int, input().split())
+
+dfs_visited = [False]*(N+1)
+bfs_visited = [False]*(N+1)
+
+graph =[[] for _ in range(N+1)]
+
+for _ in range(1,M+1):
+    node_A , node_B = map(int, input().split())
+    graph[node_A].append(node_B)
+    graph[node_B].append(node_A)
+
 
 sorted_graph = []
 for i in graph:
     sorted_graph.append(sorted(i))
-    
 
-dfs(v)
+
+dfs(V)
 print()
-bfs(v)
-
+bfs(V)
