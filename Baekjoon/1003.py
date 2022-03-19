24900e6fea8ef0716836664425cf432a0ab49abb
yukyung123 <rladbrud12@naver.com>
2022-03-19 23:26:59 +0900

    1003

--- Baekjoon/1003.py
+++ Baekjoon/1003.py
@@ -1,13 +1,18 @@
 import sys 
 input = sys.stdin.readline
 
-T = int(input())
-for i in range(T):
-    N = int(input())
-    zero = [1,0,1]
-    one = [0,1,1]
-    if N >= 3:
-        for i in range(3, N+1):
+zero = [1, 0, 1]
+one = [0, 1, 1]
+
+def fibonacci(num):
+    length = len(zero)
+    if num >= length:
+        for i in range(length, num+1):
             zero.append(zero[i-1] + zero[i-2])
             one.append(one[i-1] + one[i-2])
-    print(zero[N], one[N])
+    print(zero[num], one[num])
+
+T = int(input())
+    
+for _ in range(T):
+    fibonacci(int(input()))
--- Baekjoon/2606.py
+++ Baekjoon/2606.py
@@ -3,8 +3,8 @@ import sys
 input = sys.stdin.readline 
 
 
-n = int(input())
-m = int(input())
+n = int(input()) # 컴퓨터의 수
+m = int(input()) # 연결되어 있는 컴퓨터 쌍
 
 graph = [[] for _ in range(n+1)]
 visited = [False for _ in range(n+1)]
@@ -15,16 +15,15 @@ for i in range(m):
     graph[vertex_2].append(vertex_1)
 
 
-
 def bfs():
     queue = deque()
     count = -1
     queue.append(graph[1])
-    while queue :
+    while queue:
         x = queue.popleft()
         for i in x:
-            if not visited[int(i)] :
-                visited[int(i)] = True
+            if not visited[i]:
+                visited[i] = True
                 count += 1
                 queue.append(graph[i])
     print(count)
