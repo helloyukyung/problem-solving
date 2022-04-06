8ace2a7ac729ffd8a98576b001b534c290fc7482
yukyung123 <rladbrud12@naver.com>
2022-04-07 00:03:03 +0900

    18258

--- Baekjoon/18258.py
+++ Baekjoon/18258.py
@@ -8,25 +8,27 @@ for i in range(N):
     order = input().split()
     if order[0] == "push":
         queue.append(order[1])
-    if order[0] == "pop":
+    elif order[0] == "pop":
         if len(queue) == 0 :
             print(-1)
         else :
             print(queue.popleft()) 
-    if order[0] == "size":
+    elif order[0] == "size":
         print(len(queue))
-    if order[0] == "empty":
+    elif order[0] == "empty":
         if len(queue) == 0:
             print(1)
         else :
             print(0)
-    if order[0] == "front":
+    elif order[0] == "front":
         if len(queue) == 0:
             print(-1)
         else :
             print(queue[0])
-    if order[0] == "back":
+    elif order[0] == "back":
         if len(queue) == 0:
             print(-1)
         else :
             print(queue[-1])
+    else :
+        print("????")
