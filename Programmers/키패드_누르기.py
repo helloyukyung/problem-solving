7456d50a4944aa9d42370372c8669f248f65fdaa
yukyung123 <rladbrud12@naver.com>
2022-04-25 23:40:07 +0900

    신규_아이디_추천

--- Programmers/키패드_누르기.py
+++ Programmers/키패드_누르기.py
@@ -6,8 +6,7 @@ def solution(numbers, hand):
            4: [1, 0], 5: [1, 1], 6: [1, 2],
            7: [2, 0], 8: [2, 1], 9: [2, 2],
            '*':[3, 0], 0: [3, 1], '#': [3, 2]}
-    
-    # 시작 위치
+
     left_s = dic['*']
     right_s = dic['#']
     
@@ -16,15 +15,12 @@ def solution(numbers, hand):
         if i in [1, 4, 7]:
             answer += 'L'
             left_s = now
-
         elif i in [3, 6, 9]:
             answer += 'R'
             right_s = now
-
         else:
             left_d = 0
             right_d = 0
-            
             for a, b, c in zip(left_s, right_s, now):
                 left_d += abs(a-c)
                 right_d += abs(b-c)
@@ -45,5 +41,4 @@ def solution(numbers, hand):
                 else:
                     answer += 'R'
                     right_s = now
-            
     return answer
