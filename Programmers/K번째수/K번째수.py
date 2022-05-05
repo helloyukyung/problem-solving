fb207c4516473b526a33e44e9c6b5f4653a5e5bf
yukyung123 <rladbrud12@naver.com>
2022-05-05 18:07:32 +0900

    K번째수

---
+++ Programmers/K번째수/K번째수.py
@@ -0,0 +1,11 @@
+def solution(array, commands):
+    answer = []
+    sliced_array = []
+    picked = 0
+    for i,j,k in commands:
+        sliced_array = array[i-1:j]
+        sliced_array.sort()
+        picked = sliced_array[k-1]
+        answer.append(picked)
+        
+    return answer
