9837c01a4242182556e1a1a4202344caa893ab8b
yukyung123 <rladbrud12@naver.com>
2022-05-05 18:37:37 +0900

    폴더정리

--- Programmers/소수_만들기/소수_만들기.py
+++ Programmers/소수_만들기/소수_만들기.py
@@ -3,9 +3,9 @@ import math
 count = 0
 
 def is_prime_number(x):
-    if x == 0 or x == 1:
+    if x == 0 or x == 1: # 0이면 false
         return False
-    for i in range(2, int(math.sqrt(x))+1):
+    for i in range(2, int(math.sqrt(x))+1): #  1보다 크고 루트 n 이하인 모든 자연수들로 나누어떨어지지 않으면 소수
         if x % i == 0:
             return True
     return False 
@@ -15,6 +15,7 @@ def solution(nums):
     n = len(nums)
 
     def dfs(L, idx):
+        print(L,idx)
         global count 
         if L == 3:
             if not is_prime_number(sum(res)):
@@ -22,8 +23,12 @@ def solution(nums):
             return 
         else:
             for i in range(idx, n):
+                print( "ff",res[L] , nums[i])
                 res[L] = nums[i]
                 dfs(L+1,i+1)
     dfs(L=0, idx=0)
 
-    return count
+    return print(count)
+
+nums = [1,2,3,4]
+solution(nums)
