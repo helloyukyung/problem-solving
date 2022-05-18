831ddbbe7c9e0552009e096bf717efbc76bcbda3
yukyung123 <rladbrud12@naver.com>
2022-05-19 01:07:38 +0900

    뮤직비디오

---
+++ Inflearn/Python/뮤직비디오(결정알고리즘).py
@@ -0,0 +1,30 @@
+import sys 
+sys.stdin.readline
+
+n,m =map(int, input().split())
+music_list = list(map(int , input().split()))
+
+left = 1
+right = sum(music_list)
+res = 0
+
+def Count(capacity):
+    cnt = 1
+    sum = 0 
+    for x in music_list:
+        if sum + x > capacity:
+            cnt += 1
+            sum = x
+        else:
+            sum += x
+    return cnt
+
+while left <= right:
+    mid = (left +right ) //2
+    if Count(mid) <= m:
+        res = mid
+        right = mid -1
+    else:
+        left = mid +1
+print(res)
+
