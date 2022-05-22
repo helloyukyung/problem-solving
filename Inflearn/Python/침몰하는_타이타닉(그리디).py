84c50e3ff4c4f7959a8eaa7aaab3fb3a98281496
yukyung123 <rladbrud12@naver.com>
2022-05-22 18:17:00 +0900

    침몰하는_타이타닉

--- Inflearn/Python/침몰하는_타이타닉(그리디).py
+++ Inflearn/Python/침몰하는_타이타닉(그리디).py
@@ -4,15 +4,20 @@ input = sys.stdin.readline
 
 n,m = map(int, input().split())
 passengers = list(map(int, input().split()))
-passengers.sort()
 
+passengers.sort()
+passengers = deque(passengers)
 count = 0
 
-while len(passengers) > 0:
-    if passengers[0] + passengers[-1] <= m:
-        passengers = passengers[1:-1]
+while passengers:
+    if len(passengers) == 1:
         count += 1
-    elif passengers[0] + passengers[-1] > m:
-        passengers = passengers[:-1]
-        count += 1 
+        break
+    if passengers[0] + passengers[-1] > m:
+        passengers.pop()
+        count += 1
+    else:
+        passengers.pop()
+        passengers.popleft()
+        count += 1
 print(count)
