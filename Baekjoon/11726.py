1d14d8ab0d8fa178b8aac874f01ece522fb03cc0
yukyung123 <rladbrud12@naver.com>
2022-05-29 01:05:35 +0900

    11727

--- Baekjoon/11726.py
+++ Baekjoon/11726.py
@@ -1,10 +1,15 @@
+import sys
+input = sys.stdin.readline
+
 n = int(input())
 
-d = [0]*(n+2)
-d[1] = 1
-d[2] = 1
+dp = [0 for _ in range(n+1)]
 
-for i in range(3,n+2):
-    d[i] = d[i-1] + d[i-2]
+if n <= 3 : print(n)
+else : 
+	dp[1] = 1
+	dp[2] = 2
+	for i in range(3, n+1):
+		dp[i] = dp[i-1] + dp[i-2]
 
-print(d[n+1])
+	print(dp[i]%10007)
