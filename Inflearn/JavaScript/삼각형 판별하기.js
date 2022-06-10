1aeaa0711683bde27e9f654dda2ec3a19e525d44
yukyung123 <rladbrud12@naver.com>
2022-06-11 01:25:24 +0900

    세_수_중_최솟값

--- Inflearn/JavaScript/삼각형 판별하기.js
+++ Inflearn/JavaScript/삼각형 판별하기.js
@@ -9,3 +9,16 @@ function solution(a, b, c) {
 }
 
 console.log(solution(6, 7, 11));
+
+function solution2(a, b, c) {
+  let answer = "YES",
+    max;
+  let sum = a + b + c;
+
+  if (a > b) max = a;
+  else max = b;
+  if (max < c) max = c;
+  if (sum - max <= max) answer = "NO";
+  return answer;
+}
+console.log(solution2(6, 7, 11));
