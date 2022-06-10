5f804b2b5454e085181c48ae8ebe7a27d61d7d23
yukyung123 <rladbrud12@naver.com>
2022-06-11 01:36:02 +0900

    연필 개수

--- Inflearn/JavaScript/연필 개수.js
+++ Inflearn/JavaScript/연필 개수.js
@@ -1,10 +1,17 @@
 function solution(studentNumber) {
   let answer;
   answer = parseInt(studentNumber / 12);
-  if (studentNumber % 12 > 0) {
+  if (studentNumber % 12 !== 0) {
     answer = answer + 1;
   }
   return answer;
 }
 
 console.log(solution(178));
+
+function solution2(studentNumber) {
+  let answer = Math.ceil(studentNumber / 12);
+
+  return answer;
+}
+console.log(solution2(178));
