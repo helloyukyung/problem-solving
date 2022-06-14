d6c91a9fe3528a1f9c495ef8a98c68548eebdd0b
yukyung123 <rladbrud12@naver.com>
2022-06-14 23:35:36 +0900

    문자\ 찾기

---
+++ Inflearn/JavaScript/문자 찾기.js
@@ -0,0 +1,11 @@
+function solution(str, a) {
+  let answer = 0;
+  for (let x of str) {
+    if (x === a) {
+      answer++;
+    }
+  }
+  return answer;
+}
+let str = "COMPUTERPROGRAMMING";
+console.log(solution(str, "R"));
