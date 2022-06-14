7ea64f3c530faa63fffc797d71f44204a4782c85
yukyung123 <rladbrud12@naver.com>
2022-06-14 23:56:16 +0900

    A를\ \#으로

--- Inflearn/JavaScript/A를 #으로.js
+++ Inflearn/JavaScript/A를 #으로.js
@@ -10,3 +10,21 @@ function solution(word) {
 }
 
 console.log(solution("BANANA"));
+
+function solution2(word) {
+  let answer = "";
+  for (let x of word) {
+    if (x === "A") answer += "#";
+    else answer += x;
+  }
+  return answer;
+}
+
+console.log(solution2("BANANA"));
+
+function solution3(word) {
+  let answer = word;
+  answer = answer.replace(/A/g, "#");
+  return answer;
+}
+console.log(solution3("BANANA"));
