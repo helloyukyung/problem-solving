457a09b2da61c8079678a03f85d55db5d9045116
yukyung123 <rladbrud12@naver.com>
2022-06-17 23:57:16 +0900

    중복문자제거

--- Inflearn/JavaScript/중복문자제거.js
+++ Inflearn/JavaScript/중복문자제거.js
@@ -1,7 +1,16 @@
 function solution(str) {
   let answer = [...new Set(str)];
-  return answer.join();
+  return answer.join("");
 }
 
 let str = "ksekkset";
 console.log(solution(str));
+
+function solution2(s) {
+  let answer = "";
+  for (let i = 0; i < s.length; i++) {
+    if (s.indexOf(s[i]) === i) answer += s[i];
+  }
+  return answer;
+}
+console.log(solution2(str));
