71ddca02c21f7e7fd7f531247d5e47a47d2bc5f8
yukyung123 <rladbrud12@naver.com>
2022-06-19 23:51:36 +0900

    큰 수 출력하기

--- JavaScript/Inflearn/큰 수 출력하기.js
+++ JavaScript/Inflearn/큰 수 출력하기.js
@@ -1,7 +1,11 @@
-function solution(num) {
-  answer = [];
-  for (let i; i < num.length; i++) {}
+function solution(arr) {
+  let answer = [];
+  answer.push(arr[0]);
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > arr[i - 1]) answer.push(arr[i]);
+  }
   return answer;
 }
-num = [7, 3, 9, 5, 6, 12];
-console.log(solution(num));
+
+let arr = [7, 3, 9, 5, 6, 12];
+console.log(solution(arr));
