0d6f959240c541c51a65254370ed7e43363337f4
Yukyung Kim / Sally <rladbrud12@naver.com>
2022-10-18 01:20:32 +0900

    세\ 수\ 중\ 최솟값.js

--- JavaScript/Inflearn/세 수 중 최솟값.js
+++ JavaScript/Inflearn/세 수 중 최솟값.js
@@ -1,14 +1,14 @@
 function solution(a, b, c) {
   let answer;
-  if (a < b) {
-    answer = a;
-  } else {
+  if (a > b) {
     answer = b;
+  } else {
+    answer = a;
   }
-  if (answer < c) {
+  if (c < answer) {
     answer = c;
   }
   return answer;
 }
 
-console.log(solution(2, 5, 1));
+console.log(solution(6, 5, 11));
