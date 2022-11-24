985c1d580085892ea71d19d1e33409e8aecc0c97
Yukyung Kim / Sally <rladbrud12@naver.com>
2022-11-25 00:59:32 +0900

    숫자만_추출

--- JavaScript/Inflearn/숫자만_추출.js
+++ JavaScript/Inflearn/숫자만_추출.js
@@ -1,7 +1,7 @@
 function solution(str) {
   str = str.replace(/[^0-9]/g, "");
-  const ans = [...new Set(str)].join("");
-  return ans;
+
+  return parseInt(str);
 }
 
 console.log(solution("g0en2T0s8eSoft"));
