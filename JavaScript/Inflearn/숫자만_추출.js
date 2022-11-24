f869691894e0b30e99b1290829a0cf3cecb654a6
Yukyung Kim / Sally <rladbrud12@naver.com>
2022-11-25 01:07:54 +0900

    숫자만_추출

--- JavaScript/Inflearn/숫자만_추출.js
+++ JavaScript/Inflearn/숫자만_추출.js
@@ -1,7 +1,9 @@
 function solution(str) {
-  str = str.replace(/[^0-9]/g, "");
-
-  return parseInt(str);
+  let answer = "";
+  for (let x of str) {
+    if (!isNaN(x)) answer = answer * 10 + Number(x);
+  }
+  return answer;
 }
 
 console.log(solution("g0en2T0s8eSoft"));
