7b7b8cfe715237b025b68f5697cfbf7aefe276e6
Yukyung Kim / Sally <rladbrud12@naver.com>
2022-12-12 20:51:28 +0900

    18108

---
+++ JavaScript/Baekjoon/18108.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+
+const input = fs.readFileSync(filePath).toString();
+
+console.log(parseInt(input) - 544);
