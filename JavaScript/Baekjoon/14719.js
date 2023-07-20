00083bfc849d0284cf30a6f319b6464038659ce5
Yukyung Kim / Sally <rladbrud12@naver.com>
2023-07-21 01:16:00 +0900

    2023.07.21

--- JavaScript/Baekjoon/14719.js
+++ JavaScript/Baekjoon/14719.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().trim().split("\n");
+
 const [h, w] = input[0].split(" ").map((val) => +val);
 
 const rain = input[1].split(" ").map((val) => +val);
@@ -8,6 +9,7 @@ let water = 0;
 
 for (let i = 0; i < rain.length; i++) {
   const maxLen = Math.max(...rain.slice(0, i + 1));
+  const minLen = Math.max(...rain.slice(i));
   const minBlock = Math.min(maxLen, minLen);
   water += minBlock - rain[i];
 }
