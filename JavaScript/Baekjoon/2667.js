788fafded1121cc21ac40367715cdcef336a6e02
Yukyung Kim / Sally <rladbrud12@naver.com>
2023-07-22 18:35:26 +0900

    2023.07.22

--- JavaScript/Baekjoon/2667.js
+++ JavaScript/Baekjoon/2667.js
@@ -1,52 +1,64 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-const input = fs.readFileSync(filePath).toString().trim().split("\n");
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
 
-const n = Number(input.shift())
+const N = Number(input.shift());
 
-const map = input.map((row) => row.split("").map(Number))
-const visited = new Array(n*n).fill(false);
-
-const dx= [0,0,-1,1]
-const dy = [-1,1,0,0]
-
-const complexes = [];
-let houses = 0;
-
-function bfs (x ,y) {
-    const queue = [];
-    queue.push([x,y])
-    houses = 0;
-
-    while (queue.length) {
-        const target = queue.shift()
-        x = target[0]
-        y = target[1]
-
-        if (visited[x][y] === false) {
-            visited[x][y] = true
-            houses +=1
-            for (let i = 0; i< 4; i++) {
-                const xx = dx[i] + x
-                const yy = dy[i] + y
-                if (!(xx < 0 || xx >= n || yy < 0 || y >=n )){
-                    queue.push(xx,yy)
-                }
-            }
-
-        }
-        complexes.push(houses)
-    }
+const map = input.map((element) => {
+  return element
+    .trim()
+    .split("")
+    .map((element) => Number(element));
+});
+const visited = [];
+for (let i = 0; i < N; i++) {
+  visited.push(new Array(N).fill(0));
 }
 
-for(let i=0; i<n;i++) {
-    for (let j=0; j < n; j++) {
-        if (visited[i][j] !== false && map[i][j] === 1) {
-            bfs(i,j)
+const moveRow = [0, 0, 1, -1];
+const moveCol = [1, -1, 0, 0];
+
+const rangeCheck = (row, col) => {
+  if (row >= 0 && row < N && col >= 0 && col < N) {
+    return true;
+  } else return false;
+};
+
+function bfs(row, col) {
+  const queue = [];
+  queue.push([row, col]);
+  while (queue.length) {
+    const [row, col] = queue.shift();
+
+    if (visited[row][col] === 0) {
+      visited[row][col] = 1;
+      number++;
+      for (let n = 0; n < 4; n++) {
+        if (
+          rangeCheck(row + moveRow[n], col + moveCol[n]) &&
+          map[row + moveRow[n]][col + moveCol[n]] === 1
+        ) {
+          queue.push([row + moveRow[n], col + moveCol[n]]);
         }
+      }
     }
+  }
 }
 
-complexes.sort((a, b) => a - b);
-let answer = [complexes.length, ...complexes];
+const complex = [];
+let number = 0;
+
+for (let row = 0; row < N; row++) {
+  for (let col = 0; col < N; col++) {
+    if (Number(map[row][col]) === 1 && visited[row][col] === 0) {
+      bfs(row, col);
+      complex.push(number);
+      number = 0;
+    }
+  }
+}
+
+complex.sort((a, b) => a - b);
+const answer = [complex.length, ...complex];
+
 console.log(answer.join("\n"));
