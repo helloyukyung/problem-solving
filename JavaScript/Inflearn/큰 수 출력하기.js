9277bd0b3dd45ba89806a24197897b4f38af4774
yukyung <49505843+helloyukyung@users.noreply.github.com>
2024-04-26 22:35:53 +0900

    2024.04.26

--- JavaScript/Inflearn/큰 수 출력하기.js
+++ JavaScript/Inflearn/큰 수 출력하기.js
@@ -1,16 +1,11 @@
-function solution(arrLen, arr) {
+function solution(arr) {
   let answer = [arr[0]];
-  let max = arr[0];
-  for (let i = 0; i < arrLen; i++) {
-    if (arr[i] > max) {
-      max = arr[i];
-      answer.push(max);
-    } else {
-      max = arr[i];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > arr[i - 1]) {
+      answer.push(arr[i]);
     }
   }
-  return answer;
 }
 
 let arr = [7, 3, 9, 5, 6, 12];
-console.log(solution(arr.length, arr));
+console.log(solution(arr));
