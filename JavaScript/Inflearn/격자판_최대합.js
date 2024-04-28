e82b83b66cfcf255d466e1d8659c16f11cedef47
yukyung <49505843+helloyukyung@users.noreply.github.com>
2024-04-28 16:02:18 +0900

    2024.04.28

--- JavaScript/Inflearn/격자판_최대합.js
+++ JavaScript/Inflearn/격자판_최대합.js
@@ -1,24 +1,28 @@
-function solution(arr){
-    let answer = [0,0,0,0,0]
-    let 대각선 = [0,0]
-    for(let i=0; i<arr.length; i++) {
-        for(let j=0; j<arr.length; j++){
-            answer[i] += arr[i][j]
-            if (i==j){
-                대각선[0] += arr[i][j]
-            }
-            if (i+j === arr.length) {
-                대각선[1] += arr[i][j]
-            }
-        }
+function solution(arr) {
+  let answer = Number.MIN_SAFE_INTEGER;
+  let n = arr.length;
+  let sum1 = 0;
+  let sum2 = 0;
+  for (let i = 0; i < n; i++) {
+    sum1 = sum2 = 0;
+    for (let j = 0; j < n; j++) {
+      sum1 += arr[i][j];
+      sum2 += arr[j][i];
     }
-
-    return Math.max(...answer,...대각선);
+    answer = Math.max(answer, sum1, sum2);
+  }
+  sum1 = sum2 = 0;
+  for (let i = 0; i < n; i++) {
+    sum1 += arr[i][i];
+    sum2 += arr[i][n - i - 1];
+  }
 }
 
-let arr=[[10, 13, 10, 12, 15],
-    [12, 39, 30, 23, 11],
-    [11, 25, 50, 53, 15],
-    [19, 27, 29, 37, 27],
-    [19, 13, 30, 13, 19]];
+let arr = [
+  [10, 13, 10, 12, 15],
+  [12, 39, 30, 23, 11],
+  [11, 25, 50, 53, 15],
+  [19, 27, 29, 37, 27],
+  [19, 13, 30, 13, 19],
+];
 console.log(solution(arr));
