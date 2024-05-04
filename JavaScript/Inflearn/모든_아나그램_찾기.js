d4261a346289a02b7d7dfc199ab940f9ae0e216f
Yukyung Kim / Sally <49505843+helloyukyung@users.noreply.github.com>
2024-05-05 02:46:21 +0900

    [해쉬, 투포인터, 슬라이딩 윈도우] 모든 아나그램 찾기 리팩토링

--- JavaScript/Inflearn/모든_아나그램_찾기.js
+++ JavaScript/Inflearn/모든_아나그램_찾기.js
@@ -1,49 +1,33 @@
-function isAnagramF(sH, tH) {
-  let isAnagram = true;
-  for (let [key, value] of tH) {
-    if (value !== sH.get(key)) {
-      isAnagram = false;
-      break;
-    } else isAnagram = true;
+function compareMaps(map1, map2) {
+  if (map1.size !== map2.size) return false;
+  for (let [key, val] of map1) {
+    if (!map2.has(key) || map2.get(key) !== val) return false;
   }
-
-  return isAnagram;
+  return true;
 }
 
 function solution(s, t) {
   let answer = 0;
-  let sH = new Map();
   let tH = new Map();
-
-  sH.set("a", 0);
-  sH.set("b", 0);
-  sH.set("c", 0);
-  sH.set("A", 0);
-
+  let sH = new Map();
   for (let x of t) {
-    if (tH.has(x)) {
-      tH.set(x, tH.get(x) + 1);
-    } else {
-      tH.set(x, 1);
-    }
+    if (tH.has(x)) tH.set(x, tH.get(x) + 1);
+    else tH.set(x, 1);
   }
-  for (let i = 0; i < t.length; i++) {
-    if (sH.has(s[i])) {
-      sH.set(s[i], sH.get(s[i]) + 1);
-    } else {
-      sH.set(s[i], 1);
-    }
+  let len = t.length - 1;
+  for (let i = 0; i < len; i++) {
+    if (sH.has(s[i])) sH.set(s[i], sH.get(s[i]) + 1);
+    else sH.set(s[i], 1);
   }
-  if (isAnagramF(sH, tH)) answer++;
-  for (let i = t.length; i < s.length; i++) {
-    if (isAnagramF(sH, tH)) {
-      answer++;
-    }
-
-    sH.set(s[i], sH.get(s[i]) + 1);
-    sH.set(s[i - t.length], sH.get(s[i - t.length]) - 1);
+  let lt = 0;
+  for (let rt = len; rt < s.length; rt++) {
+    if (sH.has(s[rt])) sH.set(s[rt], sH.get(s[rt]) + 1);
+    else sH.set(s[rt], 1);
+    if (compareMaps(sH, tH)) answer++;
+    sH.set(s[lt], sH.get(s[lt]) - 1);
+    if (sH.get(s[lt]) === 0) sH.delete(s[lt]);
+    lt++;
   }
-
   return answer;
 }
 
