bd2545bc4f2bfe1542a2a895e08dd99c6bee43f0
Yukyung Kim / Sally <49505843+helloyukyung@users.noreply.github.com>
2024-05-24 02:03:08 +0900

    토마토

--- JavaScript/Baekjoon/7576.js
+++ JavaScript/Baekjoon/7576.js
@@ -2,19 +2,16 @@ const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 const fs = require("fs");
 
 const inputs = fs.readFileSync(filePath).toString().split("\n");
-let cursor = 0;
-const outputs = [];
 
-const [m, n] = inputs[cursor++].split(" ").map(Number);
+const [m, n] = inputs[0].split(" ").map(Number);
+const board = inputs.slice(1).map((input) => input.split(" ").map(Number));
 
-const field = inputs.slice(cursor).map((input) => input.split(" ").map(Number));
-
-const q = [];
+const queue = [];
 
 for (let y = 0; y < n; y++) {
   for (let x = 0; x < m; x++) {
-    if (field[y][x] === 1) {
-      q.push(x, y);
+    if (board[y][x] === 1) {
+      queue.push(x, y);
     }
   }
 }
@@ -24,26 +21,26 @@ let result = 0;
 const dx = [-1, 1, 0, 0];
 const dy = [0, 0, -1, 1];
 
-for (let i = 0, days = 1; i < q.length; days++) {
-  for (const { length } = q; i < length; ) {
-    const ox = q[i++];
-    const oy = q[i++];
+for (let i = 0, days = 1; i < queue.length; days++) {
+  for (const { length } = queue; i < length; ) {
+    const ox = queue[i++];
+    const oy = queue[i++];
 
     for (let j = 0; j < 4; j++) {
       const x = ox + dx[j];
       const y = oy + dy[j];
       if (x < 0 || x >= m || y < 0 || y >= n) continue;
-      if (field[y][x] !== 0) continue;
-      field[y][x] = 1;
+      if (board[y][x] !== 0) continue;
+      board[y][x] = 1;
       result = days;
-      q.push(x, y);
+      queue.push(x, y);
     }
   }
 }
 
 for (let y = 0; y < n; y++) {
   for (let x = 0; x < m; x++) {
-    if (field[y][x] === 0) result = -1;
+    if (board[y][x] === 0) result = -1;
   }
 }
 
