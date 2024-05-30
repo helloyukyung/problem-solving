8732f60715c23dc4b146dc9e6187230e1c480cb6
yukyung <49505843+helloyukyung@users.noreply.github.com>
2024-05-30 20:47:28 +0900

    2×n 타일링

---
+++ JavaScript/Baekjoon/11726.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filePath).toString().split("\n");
+
+const n = parseInt(input[0]);
+
+const dp = Array.from({ length: n + 1 }, () => 0);
+
+dp[1] = 1;
+
+if (n > 1) {
+  dp[2] = 2;
+}
+
+for (let i = 3; i <= n; i++) {
+  dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+}
+
+console.log(dp[n]);
