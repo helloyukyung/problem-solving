249d598e151cd930678cb7433b5c3ac4f10df699
Yukyung Kim / Sally <49505843+helloyukyung@users.noreply.github.com>
2024-06-04 00:46:32 +0900

    [DP] 가장 긴 증가하는 부분 수열

---
+++ JavaScript/Baekjoon/11053.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filePath).toString().split("\n");
+
+const n = parseInt(input[0]);
+const arr = input[1].split(" ").map(Number);
+
+let dp = Array(n).fill(1);
+
+for (let i = 0; i < n; i++) {
+  for (let j = 0; j < i; j++) {
+    if (arr[i] > arr[j]) {
+      dp[i] = Math.max(dp[i], dp[j] + 1);
+    }
+  }
+}
+
+console.log(Math.max(...dp));
