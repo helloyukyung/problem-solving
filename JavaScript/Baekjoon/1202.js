8e94c7ebf3734a1329fdc29e0021108789a62c4a
yukyung <49505843+helloyukyung@users.noreply.github.com>
2024-06-15 18:46:26 +0900

    [우선순위 큐] 보석도둑 re

--- JavaScript/Baekjoon/1202.js
+++ JavaScript/Baekjoon/1202.js
@@ -1,108 +1,97 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filePath).toString().split("\n");
+
 class MaxHeap {
   constructor() {
     this.heap = [];
   }
-  swap(a, b) {
-    // 구조분해 할당 문법으로 swap 가능
-    [this.heap[a], this.heap[b]] = [this.heap[b], this.heap[a]];
-  }
-  size() {
-    return this.heap.length;
-  }
-  push(value) {
-    // 맨뒤에 추가 max heap 이므로 부모랑 비교해서 큰값을 부모랑 swap 해줘야함
+
+  insert(value) {
     this.heap.push(value);
-    let idx = this.heap.length - 1;
-    let parent = Math.floor((idx - 1) / 2);
-
-    while (this.heap[parent] < value) {
-      this.swap(parent, idx);
-      idx = parent;
-      parent = Math.floor((idx - 1) / 2);
-    }
-    // return this.print()
-  }
-  // 큐이기 때문에 삭제는 항상 루트노드부터 이루어짐. 루트 노드를 삭제하고, 맨마지막 인덱스를 루트랑 교환
-
-  pop() {
-    const lastIdx = this.heap.length - 1;
-    let idx = 0;
-    this.swap(0, lastIdx); // 0번이 루트노드
-    let value = this.heap.pop();
-
-    while (idx < lastIdx) {
-      let leftChildIdx = idx * 2 + 1;
-      let rightChildIdx = idx * 2 + 2;
-
-      // 왼쪽자식 인덱스가 더 크다는 뜻은 자식노드가 없다는 뜻
-      if (leftChildIdx >= lastIdx) {
-        break;
-      } else if (rightChildIdx >= lastIdx) {
-        // 왼쪽 자식만 있는경우 자식과 비교해서 크면 스왑
-        if (this.heap[idx] < this.heap[leftChildIdx]) {
-          this.swap(idx, leftChildIdx);
-          idx = leftChildIdx;
-        } else {
-          break;
-        }
-      } else {
-        // 둘다 있는경우 중 두 자식이 루트보다 다 큰경우
-        if (
-          this.heap[leftChildIdx] > this.heap[idx] &&
-          this.heap[rightChildIdx] > this.heap[idx]
-        ) {
-          // 큰값이랑 스왑
-          if (this.heap[leftChildIdx] > this.heap[rightChildIdx]) {
-            this.swap(idx, leftChildIdx);
-            idx = leftChildIdx;
-          } else {
-            this.swap(idx, rightChildIdx);
-            idx = rightChildIdx;
-          }
-        } else if (this.heap[leftChildIdx] > this.heap[idx]) {
-          // 왼쪽 자식만 루트보다 클 경우
-          this.swap(leftChildIdx, idx);
-          idx = leftChildIdx;
-        } else if (this.heap[rightChildIdx] > this.heap[idx]) {
-          // 오른쪽 자식
-          this.swap(rightChildIdx, idx);
-          idx = rightChildIdx;
-        } else {
-          // 둘다 작을경우 안바꿈
-          break;
-        }
-      }
-    }
-    return value;
+    this.heapifyUp(this.heap.length - 1);
   }
 
-  print() {
-    console.log(this.heap);
+  extractMax() {
+    if (this.heap.length === 0) return null;
+    if (this.heap.length === 1) return this.heap.pop();
+    // 루트 노드를 저장
+    const root = this.heap[0];
+    // 마지막 노드를 루트 노드로 옮기고, 마지막 노드를 제거
+    // 왜 마지막 노드를 루트 노드로 옮기는가?
+    // 힙은 완전 이진 트리이기 때문에 마지막 노드가 제일 마지막에 추가된 노드이다.
+    // 따라서 마지막 노드를 루트 노드로 옮기면서 힙의 구조를 유지할 수 있다.
+    this.heap[0] = this.heap.pop();
+    // 새로운 루트 노드를 기준으로 heapifyDown
+    this.heapifyDown(0);
+
+    return root;
+  }
+
+  heapifyUp(index) {
+    // 부모 노드의 인덱스를 구하는 공식 Math.floor는 내림을 해주는 함수
+    let parentIndex = Math.floor((index - 1) / 2);
+
+    while (index > 0 && this.heap[index] > this.heap[parentIndex]) {
+      [this.heap[index], this.heap[parentIndex]] = [
+        this.heap[parentIndex],
+        this.heap[index],
+      ];
+      index = parentIndex;
+      parentIndex = Math.floor((index - 1) / 2);
+    }
+  }
+
+  heapifyDown(index) {
+    let largest = index;
+    const leftChildIndex = 2 * index + 1;
+    const rightChildIndex = 2 * index + 2;
+
+    if (
+      leftChildIndex < this.heap.length &&
+      this.heap[leftChildIndex] > this.heap[largest]
+    ) {
+      largest = leftChildIndex;
+    }
+
+    if (
+      rightChildIndex < this.heap.length &&
+      this.heap[rightChildIndex] > this.heap[largest]
+    ) {
+      largest = rightChildIndex;
+    }
+
+    if (largest !== index) {
+      [this.heap[index], this.heap[largest]] = [
+        this.heap[largest],
+        this.heap[index],
+      ];
+      this.heapifyDown(largest);
+    }
   }
 }
 
-const fs = require("fs");
-const input = fs
-  .readFileSync("./dev/stdin")
-  .toString()
-  .trim()
-  .split("\n")
-  .map((v) => v.split(" ").map(Number));
-const [N, K] = input.shift();
-let jewel = input.splice(0, N).sort((a, b) => a[0] - b[0]);
-let bag = input.map((v) => v[0]).sort((a, b) => a - b);
-let answer = 0;
-let possible = new MaxHeap();
+const [N, K] = input[0].split(" ").map(Number);
+const jewels = input.slice(1, N + 1).map((line) => line.split(" ").map(Number));
+const bags = input.slice(N + 1, N + K + 1).map(Number);
 
-let j = 0;
-for (let i = 0; i < K; i++) {
-  while (j < N && jewel[j][0] <= bag[i]) {
-    possible.push(jewel[j][1]);
-    j++;
+// 보석을 무게 기준으로 오름차순 정렬
+jewels.sort((a, b) => a[0] - b[0]);
+// 가방을 무게 기준으로 오름차순 정렬
+bags.sort((a, b) => a - b);
+
+const maxHeap = new MaxHeap();
+let totalValue = 0;
+let jewelIndex = 0;
+for (const bag of bags) {
+  while (jewelIndex < jewels.length && jewels[jewelIndex][0] <= bag) {
+    maxHeap.insert(jewels[jewelIndex][1]);
+    jewelIndex++;
   }
-  if (possible.size()) {
-    answer += possible.pop();
+
+  const maxVal = maxHeap.extractMax();
+  if (maxVal !== null) {
+    totalValue += maxVal;
   }
 }
-
-console.log(answer);
+console.log(totalValue);
