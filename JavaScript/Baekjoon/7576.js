84aa5aa9dfac58a996d763231b4106b8cd4fa077
yukyung <49505843+helloyukyung@users.noreply.github.com>
2024-06-21 00:55:18 +0900

    [BFS] 토마토

--- JavaScript/Baekjoon/7576.js
+++ JavaScript/Baekjoon/7576.js
@@ -1,47 +1,49 @@
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 const fs = require("fs");
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
 
-const inputs = fs.readFileSync(filePath).toString().split("\n");
+const [X, Y] = input.shift().split(" ").map(Number);
+let queue = [];
+let days = 0;
 
-const [m, n] = inputs[0].split(" ").map(Number);
-const board = inputs.slice(1).map((input) => input.split(" ").map(Number));
+const tomatoes = input.map((val) => val.split(" ").map(Number));
 
-const queue = [];
+tomatoes.map((val, i) => {
+  val.map((va, j) => {
+    if (va == 1) {
+      queue.push([i, j]);
+    }
+  });
+});
 
-for (let y = 0; y < n; y++) {
-  for (let x = 0; x < m; x++) {
-    if (board[y][x] === 1) {
-      queue.push(x, y);
+const directions = [
+  [1, 0],
+  [-1, 0],
+  [0, 1],
+  [0, -1],
+];
+
+while (queue.length > 0) {
+  // nextQueue: 다음 날 익을 토마토의 위치를 저장하는 큐
+  let nextQueue = [];
+  for (let [y, x] of queue) {
+    for (let [dy, dx] of directions) {
+      const ny = y + dy,
+        nx = x + dx;
+      if (ny >= 0 && ny < Y && nx >= 0 && nx < X) {
+        if (tomatoes[ny][nx] === 0) {
+          tomatoes[ny][nx] = 1;
+          nextQueue.push([ny, nx]);
+        }
+      }
     }
   }
-}
-
-let result = 0;
-
-const dx = [-1, 1, 0, 0];
-const dy = [0, 0, -1, 1];
-
-for (let i = 0, days = 1; i < queue.length; days++) {
-  for (const { length } = queue; i < length; ) {
-    const ox = queue[i++];
-    const oy = queue[i++];
-
-    for (let j = 0; j < 4; j++) {
-      const x = ox + dx[j];
-      const y = oy + dy[j];
-      if (x < 0 || x >= m || y < 0 || y >= n) continue;
-      if (board[y][x] !== 0) continue;
-      board[y][x] = 1;
-      result = days;
-      queue.push(x, y);
-    }
+  if (nextQueue.length > 0) {
+    days++;
+    queue = nextQueue;
+  } else {
+    break;
   }
 }
 
-for (let y = 0; y < n; y++) {
-  for (let x = 0; x < m; x++) {
-    if (board[y][x] === 0) result = -1;
-  }
-}
-
-console.log(result);
+console.log(tomatoes.join(" ").includes(0) ? -1 : days);
