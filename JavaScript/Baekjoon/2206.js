7a540a63e0e44e4d70932a5c806ecb00b8367b57
Yukyung Kim / Sally <49505843+helloyukyung@users.noreply.github.com>
2024-06-25 01:16:07 +0900

    [BFS] 벽 부수고 이동하기

--- JavaScript/Baekjoon/2206.js
+++ JavaScript/Baekjoon/2206.js
@@ -4,47 +4,45 @@ const input = fs.readFileSync(filePath).toString().trim().split("\n");
 
 let [N, M] = input[0].split(" ").map(Number);
 let board = input.slice(1).map((v) => v.split("").map(Number));
-const check = Array.from(new Array(N), () => new Array());
-const dx = [1, 0, -1, 0];
-const dy = [0, 1, 0, -1];
+const visited = Array.from(Array(N), () => Array(M).fill(false));
 const queue = [];
 
-for (let i = 0; i < N; i++) {
-  for (let j = 0; j < M; j++) {
-    check[i][j] = new Array(2).fill(0);
+const directions = [
+  [1, 0],
+  [-1, 0],
+  [0, 1],
+  [0, -1],
+];
+
+let answer = 0;
+
+const isValid = (x, y) => x >= 0 && x < N && y >= 0 && y < M;
+
+queue.push([0, 0, 1, 0]);
+visited[0][0] = true;
+
+let head = 0;
+while (head < queue.length) {
+  let [x, y, jump, isBreak] = queue[head++];
+  if (x === N - 1 && y === M - 1) {
+    answer = jump;
+    break;
   }
-}
 
-queue.push([0, 0, 0]);
-check[0][0][0] = 1;
+  for (let [dx, dy] of directions) {
+    let nx = x + dx;
+    let ny = y + dy;
 
-function BFS() {
-  let idx = 0;
-
-  while (idx !== queue.length) {
-    const [y, x, isBreak] = queue[idx];
-
-    if (x === M - 1 && y === N - 1) {
-      return check[y][x][isBreak];
-    }
-
-    for (let i = 0; i < dx.length; i++) {
-      const [nx, ny] = [x + dx[i], y + dy[i]];
-
-      if (nx >= 0 && nx < M && ny >= 0 && ny < N) {
-        if (board[ny][nx] === 0 && check[ny][nx][isBreak] === 0) {
-          check[ny][nx][isBreak] = check[y][x][isBreak] + 1;
-          queue.push([ny, nx, isBreak]);
-        } else if (board[ny][nx] === 1 && isBreak === 0) {
-          check[ny][nx][isBreak + 1] = check[y][x][isBreak] + 1;
-          queue.push([ny, nx, isBreak + 1]);
-        }
+    if (isValid(nx, ny)) {
+      if (board[nx][ny] === 0 && !visited[nx][ny]) {
+        visited[nx][ny] = true;
+        queue.push([nx, ny, jump + 1, isBreak]);
+      } else if (board[nx][ny] === 1 && !isBreak) {
+        visited[nx][ny] = true;
+        queue.push([nx, ny, jump + 1, 1]);
       }
     }
-    idx++;
   }
-
-  return -1;
 }
 
-console.log(BFS());
+console.log(answer === 0 ? -1 : answer);
