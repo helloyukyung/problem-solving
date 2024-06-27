999f83676ba1bd118fb4e28225addb3611c75f73
Yukyung Kim / Sally <49505843+helloyukyung@users.noreply.github.com>
2024-06-28 00:48:09 +0900

    [그리디] 뒤집기

--- JavaScript/Baekjoon/1439.js
+++ JavaScript/Baekjoon/1439.js
@@ -2,28 +2,14 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().split(" ");
 
-let arr0 = [];
-let arr1 = [];
+let count = 0;
+input = input[0].split("");
+let prev = input[0];
 
-let arr = input[0];
-let temp = 0;
-for (let i = 0; i <= arr.length; i++) {
-  if ((i === 0 || arr[i] === arr[i - 1]) && arr.length !== i) {
-    temp++;
-  } else {
-    if (arr[i - 1] === "1") {
-      arr1.push(temp);
-    } else {
-      arr0.push(temp);
-    }
-    temp = 1;
+for (let i = 1; i < input.length; i++) {
+  if (prev !== input[i]) {
+    count++;
+    prev = input[i];
   }
 }
-
-if (arr0.length > arr1.length) {
-  // 1뒤집기
-  console.log(arr1.length);
-} else {
-  // 0뒤집기
-  console.log(arr0.length);
-}
+console.log(Math.floor(count / 2));
